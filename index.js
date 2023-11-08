8e7deda8f432e180a1ef5c469f88dd04cc6e5242
uNBigode <144962752+uNBigode@users.noreply.github.com>
2023-11-08 20:02:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Seleciona o botão pelo ID
+var botao = document.getElementById('a drum');
+
+// Adiciona um evento de clique ao botão
+botao.addEventListener('click', function() {
+    alert('Botão clicado no VSCode!');
